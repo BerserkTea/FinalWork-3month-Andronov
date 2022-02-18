2d2b99d9247a6a4794c24cc955bed04e3b7357f9
«AndronovIvan» <andronoviv4@gmail.com>
2022-02-18 16:14:59 +0300

    1st tests show that programm doesnt sort correctrly, or may be its create wrong leght sorted array

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,40 +3,40 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 Console.Clear();
-string [] arrayStrings = ["hello", "2", "world", ":-)" , "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"];
+string[] arrayStrings = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
 
-string [] SortStringArrayLess4Digits (string [] stringtosort)
+string[] SortStringArrayLess4Digits(string[] stringtosort)
 {
     int counterDigits = 0;
     for (int i = 0; i < stringtosort.Length; i++)
     {
-        char[] temp = stringtosort[i].ToCharArray;
-        if (temp.Length<4)
+        if (stringtosort[i].Length < 4)
         {
             counterDigits++;
         }
     }
 
-    string [] sortedArray = new string [counterDigits];
+    string[] sortedArray = new string[counterDigits];
     for (int j = 0; j < stringtosort.Length; j++)
     {
         int digitCounterSortedArray = 0;
-        char[] tempNext = stringtosort[i].ToCharArray;
-        if (tempNext.Length<4)
+        if (stringtosort[j].Length < 4)
         {
-            sortedArray[digitCounterSortedArray]=stringtosort[i];
+            sortedArray[digitCounterSortedArray] = stringtosort[j];
             digitCounterSortedArray++;
         }
     }
     return sortedArray;
 }
-void PrintStringArray (string[] stringarray)
+void PrintStringArray(string[] stringarray)
 {
     for (int i = 0; i < stringarray.Length; i++)
     {
-    
+
         Console.Write($"{stringarray[i]} ");
-    
-    Console.WriteLine();
+
+        Console.WriteLine();
     }
 }
+PrintStringArray(arrayStrings);
+PrintStringArray(SortStringArrayLess4Digits(arrayStrings));
